aff7155ef42d93afe2106126e8232ae63ca3b7d5
Prashant Bhople <mr.prashantbhople@gmail.com>
2025-11-12 23:46:33 +0530

    Updated slider to display 3 testimonials at once and improved styling

--- js/testimonial-slider.js
+++ js/testimonial-slider.js
@@ -1,6 +1,6 @@
 /*JQuery code for testimonial slider*/
 
-jQuery(document).ready(function ($) {
+/*jQuery(document).ready(function ($) {
     $('.ts-slider').slick({
         dots: true,
         arrows: false,
@@ -8,4 +8,31 @@ jQuery(document).ready(function ($) {
         autoplaySpeed: 3000,
         adaptiveHeight: true,
     });
+}); */
+
+jQuery(document).ready(function ($) {
+    $('.ts-slider').slick({
+        slidesToShow: 3,        // ✅ Show 3 testimonials at a time
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        dots: true,
+        arrows: false,
+        infinite: true,
+        responsive: [
+            {
+                breakpoint: 1024, // For tablets or medium screens
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 768, // For mobile devices
+                settings: {
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
 });
+
